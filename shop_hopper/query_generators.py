7b83e51ad9b9baaac0137dad76e4e20a5406d799
Pavel Soroka <40a.pvl@gmail.com>
2024-11-11 10:56:09 +0200

    Add comments to shop_hopper/query_generators.py

--- shop_hopper/query_generators.py
+++ shop_hopper/query_generators.py
@@ -1,12 +1,17 @@
 import urllib.parse
 
 
+# A dictionary of query generators for different platforms.
+# Each platform has a corresponding lambda function that generates a search URL
+# based on the given request string.
 query_generators = {
     'alib': lambda request:
         f'https://alib.top/find3.php?tfind={encode_alib_request(request)}',
+
     'newauction': lambda request:
         f'https://newauction.org/listing/offer/search_'
         f'{urllib.parse.quote_plus(request)}',
+
     'olx': lambda request:
         f'https://www.olx.ua/uk/list/q-{request.replace(" ", "-")}/'
 }
