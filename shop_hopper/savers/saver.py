9e74fab73d76784dd665121936ee848529c08d3f
Pavel Soroka <40a.pvl@gmail.com>
2024-11-11 10:53:59 +0200

    Add comments to shop_hopper.savers.saver.Saver

--- shop_hopper/savers/saver.py
+++ shop_hopper/savers/saver.py
@@ -2,6 +2,25 @@ from abc import ABC, abstractmethod
 
 
 class Saver(ABC):
+    """
+    Abstract base class for saving data.
+
+    Child classes must implement the `save()` method to handle saving data
+    in a specific format (e.g., CSV, JSON, database).
+    """
+
     @abstractmethod
     def save(self, data, query, logger, path='.'):
+        """
+        Saves the data to a file or other destination, defined by the subclass.
+
+        Should be implemented in the subclass.
+
+        Args:
+            data (list[dict]): The data to be saved (a list of dictionaries).
+            query (str): The query string used to fetch the data.
+            logger (Logger): An instance of a logger for logging the process.
+            path (str, optional): The directory path where to save the data .
+                                  Defaults to the current directory ('.').
+        """
         pass
