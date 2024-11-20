1f212dfd75e0831700130c9322c0df0e1acd399a
Pavel Soroka <40a.pvl@gmail.com>
2024-11-20 16:53:58 +0200

    Rename shop_hopper/via_selenium_fetch_content.py -> shop_hopper/via_selenium_content_fetcher.py

--- shop_hopper/content_fetcher.py
+++ shop_hopper/content_fetcher.py
@@ -2,7 +2,7 @@ from shop_hopper.config.platforms import (
     VIA_SELENIUM_FETCH_CONTENT_PLATFORMS,
     resolve_platform_alias
 )
-from shop_hopper.via_selenium_fetch_content import \
+from shop_hopper.via_selenium_content_fetcher import \
     ContentFetcher as SeleniumContentFetcher
 from shop_hopper.response_fetcher import ResponseFetcher
 
